551b182eaac7582806b35d5dce247f8c09c8f83a
Dario Presutti <dariopresde@protonmail.com>
2023-05-17 22:01:00 +0200

    added grab scroll

--- src/components/horizontal-timeline/horizontal-timeline.tsx
+++ src/components/horizontal-timeline/horizontal-timeline.tsx
@@ -6,46 +6,53 @@ export default component$(() => {
     
     const timelineContext= useContext(TIMELINECONTEXT);
     const timelineStateClass = useSignal<string | null>(null);
+    const circleDiameter = 120
 
     useVisibleTask$(({ track }) => {
-      const ageSelected = track(() => timelineContext.ageSelected)
-      timelineContext.state = !ageSelected ? 'open' : 'closed'
+      const yearSelected = track(() => timelineContext.yearSelected)
+      timelineContext.state = !yearSelected ? 'open' : 'closed'
 
     })
 
     useVisibleTask$(({ track }) => {
       const timelineState = track(() => timelineContext.state);
-      switch(timelineState) {
-        case 'closed':
-          timelineStateClass.value = styles.closed;
-          break;
-        case 'open':
-          timelineStateClass.value = styles.open;
-          break;
-
-
-      }  
-      if(timelineState == 'open') {
-        timelineContext.hidden = false;
-      } else {
-      setTimeout(() => {
-          timelineContext.hidden = true;
+      requestAnimationFrame(() => {
+        switch(timelineState) {
+          case 'closed':
+            timelineStateClass.value = styles.closed;
+            break;
+          case 'open':
+            timelineStateClass.value = styles.open;
+            break;
+        }  
+        if(timelineState == 'open') {
+          timelineContext.hidden = false;
+        } else {
+          setTimeout(() => {
+            timelineContext.hidden = true;
           }, 2000)
-      }
+        }
+      })
     })
     
     return (
         <>
-          <div class={styles['timeline-container']}>
-            <span>{timelineContext.ageSelected}</span>
+          <span>{timelineContext.yearSelected}</span>
+          <div 
+            style={{
+              '--circle-diameter': `${circleDiameter}px`,  
+              'width': `${( timelineContext.years.length + 1 ) * ( circleDiameter + 100 )}px` 
+            }} 
+            class={styles['timeline-container']}
+          >
             <div class={[
-            styles.timeline,
-            timelineStateClass.value,
-            timelineContext.hidden ? styles.hidden : styles.visible
+              styles.timeline,
+              timelineStateClass.value,
+                timelineContext.hidden ? styles.hidden : styles.visible
             ]}>
-            {timelineContext.ages.map((age) => (
-                  <Circle selected={ age == timelineContext.ageSelected} clickHandler$={() =>  { timelineContext.ageSelected = age } }/>
-                  ))}    
+            {timelineContext.years.map((year) => (
+              <Circle year={year} selected={ year == timelineContext.yearSelected} clickHandler$={() =>  {timelineContext.yearSelected = year } }/>
+            ))}    
             </div>
           </div>
         </>
@@ -53,20 +60,23 @@ export default component$(() => {
 });
 
 interface CircleProps {
+  year: string;
   selected: boolean
   clickHandler$: PropFunction<() => void>
 }
 
 
-const Circle = component$<CircleProps>(({selected, clickHandler$}) => {
+const Circle = component$<CircleProps>(({year, selected, clickHandler$}) => {
 
         
     return (
       <>
         <div 
-          onClick$={clickHandler$}  
+          onClick$={(ev) => {clickHandler$(); ev.stopPropagation()}}  
           class={[ styles.circle, {[styles.selected]: selected} ]}
-        ></div>
+        >
+          {year}
+        </div>
       </>
     )
 })
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,27 +2,43 @@ import type { DocumentHead  } from '@builder.io/qwik-city';
 import { routeLoader$ } from '@builder.io/qwik-city';
 import HorizontalTimeline from '~/components/horizontal-timeline/horizontal-timeline';
 import styles from './index.module.css';
-import { component$, createContextId, useContextProvider, useStore } from '@builder.io/qwik';
+import { component$, createContextId, useContextProvider, useSignal, useStore, useTask$, useVisibleTask$  } from '@builder.io/qwik';
+import { isServer } from '@builder.io/qwik/build'
 
-export const ages=
+export const years=
 [
-  '1980',
-  '1985',
-  '1990',
-  '2011'
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  '11',
+  '12',
+  '13',
+  '14',
+  '15',
+  '16'
+
+
+
 ]
 
 export type HorizontalTimelineState = 'closed' | 'open'
 
 export const useAgesLoader = routeLoader$(() => {
-  return ages;
+  return years;
 });
 
 
 export const TIMELINECONTEXT = createContextId<
   {
-    ages: string[];
-    ageSelected:string | null; 
+    years: string[];
+    yearSelected:string | null; 
     state: HorizontalTimelineState;
     hidden: boolean;
   }
@@ -31,21 +47,123 @@ export const TIMELINECONTEXT = createContextId<
  
 export default component$(() => {
 
-      const timelineContext = useStore(
+    const timelineContext = useStore(
       {
-        ages: useAgesLoader().value,
-        ageSelected: null,
+        years: useAgesLoader().value,
+        yearSelected: null,
         state: 'closed' as HorizontalTimelineState,
         hidden: true
       }
     );
 
     useContextProvider(TIMELINECONTEXT, timelineContext);
+    
+    const mouseDown = useSignal(false);
+    const posX = useSignal<number>(0);
+    const containerRef = useSignal<Element>();
+    const scrollLeft = useSignal<number>(0);
+    const scrollTo = useSignal<number>(0);
+    const scrollingDirection = useSignal<'left' | 'right' | null>();
+    const scroll = useSignal<boolean>(false);
+    const scrollBarPos = useSignal<number>(0);
+    const maxScrollLeft = useSignal<number>(0);
 
+
+
+
+    useVisibleTask$(({ track }) => {
+      const state = track(() => timelineContext.state);
+      if(containerRef.value) {
+        
+        maxScrollLeft.value = containerRef.value.scrollWidth - containerRef.value.clientWidth
+
+        requestAnimationFrame(() => {
+          if(state == 'closed' && containerRef.value) {
+            scrollTo.value = 0;
+          }
+        })
+      }
+    })
+
+    useTask$(({ track, cleanup}) => {
+      const posToScroll = track(() => scrollTo.value);
+      if(isServer) {
+        return;
+      }
+      console.log('scrollTo',scrollTo.value)
+      if(containerRef.value) {
+        scroll.value = true;
+      }
+      cleanup(() => scroll.value = false);
+    })
+
+    useVisibleTask$(({ track, cleanup }) => {
+      const scrolling = track(() => scroll.value)
+      const  id = setInterval(() => {
+        console.log('scrolling', scrolling);
+        console.log('direction', scrollingDirection.value);
+        if(!scrolling) clearInterval(id);
+        if(containerRef.value) {
+          if(scrollingDirection.value == 'left') {
+            containerRef.value.scrollLeft = containerRef.value.scrollLeft - 2 
+          } else {
+            containerRef.value.scrollLeft = containerRef.value.scrollLeft + 2 
+          }
+
+          scrollBarPos.value = containerRef.value.scrollLeft
+
+        }
+      }, 1)
+      cleanup(() => clearInterval(id))
+    })
+
+    useTask$(({ track }) => {
+      const pos = track(() => scrollBarPos.value);
+      if(isServer) {
+        return;
+      }
+      console.log('scrollbaposr', pos);
+      console.log('scrollto', scrollTo.value)
+        if(pos == scrollTo.value || pos == 0 || pos > maxScrollLeft.value) {
+          scroll.value = false;
+        }
+        if(scrollingDirection.value == 'right' && scrollTo.value < pos) {
+          scroll.value = false
+        }
+        if(scrollingDirection.value == 'left' && scrollTo.value > pos) {
+          scroll.value = false
+        }
+
+    })
+
+
+
+    
     return (
         <>
-          <div class={styles.container}>
-            <HorizontalTimeline />                 
+          <div 
+            ref={containerRef}
+            onMouseDown$={(ev, currentTarget) => {
+              posX.value = ev.clientX;
+              scrollLeft.value = currentTarget.scrollLeft
+              currentTarget.style.cursor = 'grabbing';
+              currentTarget.style.userSelect = 'none';
+              mouseDown.value = true;
+            }} 
+            onMouseUp$={(ev, currentTarget) => {
+              mouseDown.value = false;
+              currentTarget.style.cursor = 'grab';
+              currentTarget.style.removeProperty('user-select');
+            }}
+            onMouseMove$={(ev, currentTarget) => {
+              if(mouseDown.value) {
+                const dx = ev.clientX - posX.value;
+                scrollingDirection.value = dx > 0 ? 'left' : 'right'  ;
+                scrollTo.value = scrollLeft.value - dx;
+              }
+            }}
+            class={styles.container}>
+              <HorizontalTimeline />                 
           </div>
         </>
     );
